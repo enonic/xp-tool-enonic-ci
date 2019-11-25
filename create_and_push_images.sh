a027ee95048505a42fa2be8f4d3efd00e6fbf582
Guðmundur Björn Birkisson <gbi@enonic.com>
2019-11-25 14:31:39 +0100

    Update script for bash 3

--- create_and_push_images.sh
+++ create_and_push_images.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 DOCKER_REPOSITORY="enonic/enonic-ci"
-XP_DISTRO_VERSIONS="7.0.3 7.1.1" # Only use 1 of each minor version
 ENONIC_CLI_VERSION="1.0.12"
+XP_DISTRO_VERSIONS=( # Only use 1 of each minor version
+    '7.0.3'
+    '7.1.1'
+)
 
-declare -A BASE_IMAGES
 # Here you can add more base images to build. Key in the map is the base
 # image tag and the value in the map is the postfix added to the final
 # image tag.
 BASE_IMAGES=(
-    ["circleci/buildpack-deps:stable"]=""
-#    ["circleci/node"]="-node"
+    'circleci/buildpack-deps:stable::'
+#    'circleci/node::-node'
 )
 
 function build_and_push() {
     # Create tag
     IMG=$2:$(echo $3 | awk -F"." '{print $1"."$2}')$4
 
-    # Pull base image and build our image
-    docker pull $1
+    # Build our image
     docker build \
+    --no-cache \
+    --pull \
     --build-arg ENONIC_CLI_VERSION=$ENONIC_CLI_VERSION \
     --build-arg ENONIC_DISTRO_VERSION=$3 \
     --build-arg BASE_DOCKER_IMAGE=$1 \
@@ -32,10 +35,10 @@ function build_and_push() {
     docker push $IMG
 }
 
-for BASE in "${!BASE_IMAGES[@]}"
+for BASE in "${BASE_IMAGES[@]}"
 do
-    for DISTRO in $XP_DISTRO_VERSIONS
+    for DISTRO in "${XP_DISTRO_VERSIONS[@]}"
     do
-        build_and_push $BASE $DOCKER_REPOSITORY $DISTRO ${BASE_IMAGES[$BASE]}
+        build_and_push ${BASE%%::*} $DOCKER_REPOSITORY $DISTRO ${BASE##*::}
     done
 done
