a492d59aaa7e8484ad849a0bd15ceac9d056e2df
Jørgen Sivesind <jsi@enonic.com>
2020-09-11 13:29:18 +0200

    Released v7.4.1

--- create_and_push_images.sh
+++ create_and_push_images.sh
@@ -7,7 +7,7 @@ ENONIC_CLI_VERSION="1.3.0"
 XP_DISTRO_VERSIONS=( # Only use 1 of each minor version
     '7.2.4'
     '7.3.2'
-    '7.4.0'
+    '7.4.1'
 )
 
 # Here you can add more base images to build. Key in the map is the base
