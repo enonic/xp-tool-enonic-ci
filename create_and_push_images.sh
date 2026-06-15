2542b4f1f0d061d28f18db348adc8de54e2b7169
Jørgen Sivesind <jsi@enonic.com>
2026-06-15 14:27:15 +0200

    Released v7.16.5

--- create_and_push_images.sh
+++ create_and_push_images.sh
@@ -3,9 +3,9 @@
 set -ex
 
 DOCKER_REPOSITORY="enonic/enonic-ci"
-ENONIC_CLI_VERSION="4.0.0-RC1"
+ENONIC_CLI_VERSION="3.4.0"
 XP_DISTRO_VERSIONS=( # Only use 1 of each feature version
-    '8.0.0-RC1'
+    '7.16.5'
 )
 
 # Here you can add more base images to build. Key in the map is the base
